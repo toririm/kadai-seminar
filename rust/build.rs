d67b21f6f63586f9bbd8f63cddd84545b8c24a11
toririm <longtimedragon@icloud.com>
2025-01-16 17:37:01 +0900

    update

--- rust/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    println!(
-        "cargo:rustc-link-search=native={}",
-        std::env::var("VCPKG_ROOT").unwrap() + r"\installed\x64-windows\lib"
-    );
-    println!("cargo:rustc-link-lib=dylib=openblas");
-}
